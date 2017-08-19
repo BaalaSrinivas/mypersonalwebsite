327baeb3ed10d8149a7d6b732be609f994ebae78
BaalaSrinivas <contactbalasrinivas@gmail.com>
2017-08-19 11:15:07 +0530

    Skills and experience initial

--- Dependent Scripts/Angular/controller.js
+++ Dependent Scripts/Angular/controller.js
@@ -1,7 +1,7 @@
 var application = angular.module("application",[]);
 
 var controller = application.controller("SkillsController",function($scope,$http){
-    $http.get("http://192.168.0.113:99/siteapi/skills").then(function(result){
+    $http.get("http://localhost:99/siteapi/skills").then(function(result){
         $scope.ImageObjectList = result.data;
     });
 });
