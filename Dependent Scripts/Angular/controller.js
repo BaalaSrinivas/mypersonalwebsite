0e90de27a9f8eea9cf304d501ab80d8b11b4db0f
Baala Srinivas K <contactbalasrinivas@gmail.com>
2018-05-09 23:19:48 +0530

    Add api link to dynamic skill population

--- Dependent Scripts/Angular/controller.js
+++ Dependent Scripts/Angular/controller.js
@@ -1,7 +1,21 @@
 var application = angular.module("application",[]);
 
 var controller = application.controller("SkillsController",function($scope,$http){
-    $http.get("http://localhost:99/siteapi/skills").then(function(result){
-        $scope.ImageObjectList = result.data;
+    $http.get("https://baalaapi.herokuapp.com/SkillsSelect").then(function(result){
+        $scope.SkillsList = result.data;
+		
     });
+	
+	$scope.Style = function(val){
+			return { "width" : (val*10) +"%" };
+		}
+});
+
+var skillsInsert = application.controller("skillsinsertcontroller", function($scope,$http){
+	
+	$scope.submitfun = function(skill){
+	$http.post("https://baalaapi.herokuapp.com/SkillsInsert", skill, {"content-type":"application/json"}).then(function(result){
+		alert(result);
+	});
+	}
 });
